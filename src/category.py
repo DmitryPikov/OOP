8b77677f472102479c85204e57e1913f7d1cc4b8
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-05-05 21:06:37 +0400

    Fix module category.py and README.MD.

--- src/category.py
+++ src/category.py
@@ -5,9 +5,9 @@ class Category:
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name: str, description: str, product: list, category_count: int=0, product_count: int=0):
+    def __init__(self, name: str, description: str, product: list):
         self.name = name
         self.description = description
         self.products = product
         Category.category_count += 1
-        Category.product_count += 1
+        Category.product_count += len(product)
