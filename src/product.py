602bba7f571f95f197d3dfabc9defaff2fe2c18e
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-05-09 19:16:38 +0400

    Add new functions category.py and product.py

--- src/product.py
+++ src/product.py
@@ -1,11 +1,31 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product: dict):
+        return cls(
+            name=product['name'],
+            price=product['price'],
+            description=product['description'],
+            quantity=product['quantity']
+        )
--- tests/test_product.py
+++ tests/test_product.py
@@ -6,3 +6,38 @@ def test_product_init(product1: Product) -> None:
     assert product1.description == "256GB, Серый цвет, 200MP камера"
     assert product1.price == 180000.0
     assert product1.quantity == 5
+
+
+def test_price_setter_positive():
+    product = Product("Телефон", "Смартфон", 50000.0, 10)
+
+    product.price = 45000.0
+
+    assert product.price == 45000.0
+
+
+def test_price_setter_non_positive(capsys):
+    product = Product("Телефон", "Смартфон", 50000.0, 10)
+
+    product.price = -100.0
+
+    assert product.price == 50000.0
+
+    captured = capsys.readouterr()
+    assert "Цена не должна быть нулевая или отрицательная" in captured.out
+
+
+def test_new_product() -> None:
+    product_data = {
+        "name": "iPhone 15 Pro",
+        "description": "512GB, Titanium, Triple camera",
+        "price": 210000.0,
+        "quantity": 3
+    }
+
+    new_product = Product.new_product(product_data)
+
+    assert new_product.name == product_data["name"]
+    assert new_product.description == product_data["description"]
+    assert new_product.price == product_data["price"]
+    assert new_product.quantity == product_data["quantity"]
